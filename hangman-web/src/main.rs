08ebe58e3d9e0eb30a59fa44351ba04728754ce0
Lennart Kloock <lennart.kloock@protonmail.com>
2023-02-02 18:13:21 +0100

    Add simple hangman game

--- hangman-web/src/main.rs
+++ hangman-web/src/main.rs
@@ -1,13 +1,42 @@
+#![allow(non_snake_case)]
+
 use dioxus::prelude::*;
+use fermi::{AtomRef, use_atom_ref, use_init_atom_root};
+use log::{debug, Level};
 
 fn main() {
+    console_log::init_with_level(Level::Debug).expect("Error initializing logger");
     dioxus_web::launch(root);
 }
 
+static LETTERS: AtomRef<Vec<char>> = |_| vec![];
+
 fn root(cx: Scope) -> Element {
+    use_init_atom_root(cx);
+
+    let letters = use_atom_ref(cx, LETTERS);
+
+    let value = use_state(cx, || "");
+
     cx.render(rsx!(
         h1 { "Hangman" }
-        Word { word: "Test" }
+        Word { word: "Hangman" }
+        form {
+            prevent_default: "onsubmit",
+            onsubmit: move |evt| {
+                debug!("On submit: {:?}", evt);
+                if let Some(Some(c)) = evt.values.get("letter").map(|s| s.chars().nth(0)) {
+                    letters.write().push(c.to_ascii_lowercase());
+                    value.set("");
+                }
+            },
+            input {
+                name: "letter",
+                value: "{value}",
+                r#type: "text",
+                maxlength: 1,
+            }
+        }
     ))
 }
 
@@ -16,8 +45,23 @@ struct WordProps<'a> {
     word: &'a str,
 }
 
-fn Word(cx: Scope<WordProps>) -> Element {
+fn Word<'a>(cx: Scope<'a, WordProps<'a>>) -> Element<'a> {
+    let letters = use_atom_ref(cx, LETTERS);
+
+    let rendered_word: String = cx.props.word.chars().map(|c| {
+        if letters.read().contains(&c.to_ascii_lowercase()) {
+            c
+        } else {
+            '_'
+        }
+    }).collect();
+
     cx.render(rsx!(
-        p { cx.props.word }
+        p {
+            b {
+                style: "letter-spacing: .2rem;",
+                rendered_word
+            }
+        }
     ))
 }
