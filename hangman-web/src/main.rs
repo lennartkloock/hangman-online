b467657c766a3ab829ca0cd288e7d3e0dbd66485
Lennart Kloock <lennart.kloock@protonmail.com>
2023-02-03 15:28:14 +0100

    Setup dioxus-router

--- hangman-web/src/main.rs
+++ hangman-web/src/main.rs
@@ -1,32 +1,42 @@
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
-use fermi::{AtomRef, use_atom_ref, use_init_atom_root};
+use dioxus_router::{Route, Router};
+use fermi::{use_atom_ref, use_init_atom_root, AtomRef};
 use log::{debug, Level};
 
 fn main() {
-    console_log::init_with_level(Level::Debug).expect("Error initializing logger");
-    dioxus_web::launch(root);
+    console_log::init_with_level(Level::Trace).expect("Error initializing logger");
+    dioxus_web::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    use_init_atom_root(cx);
+
+    cx.render(rsx!(
+        Router {
+            style { include_str!("../out/output.css") }
+            h1 { "Hangman" }
+            Route { to: "/game", self::game {} }
+            Route { to: "", h1 { "Not Found" } }
+        }
+    ))
 }
 
 static LETTERS: AtomRef<Vec<char>> = |_| vec![];
 
-fn root(cx: Scope) -> Element {
-    use_init_atom_root(cx);
-
+fn game(cx: Scope) -> Element {
     let letters = use_atom_ref(cx, LETTERS);
 
     let value = use_state(cx, || "");
 
     cx.render(rsx!(
-        style { include_str!("../out/output.css") }
-        h1 { "Hangman" }
         Word { word: "Hangman" }
         form {
             prevent_default: "onsubmit",
             onsubmit: move |evt| {
                 debug!("On submit: {:?}", evt);
-                if let Some(Some(c)) = evt.values.get("letter").map(|s| s.chars().nth(0)) {
+                if let Some(c) = evt.values.get("letter").and_then(|s| s.chars().next()) {
                     letters.write().push(c.to_ascii_lowercase());
                     value.set("");
                 }
@@ -49,13 +59,18 @@ struct WordProps<'a> {
 fn Word<'a>(cx: Scope<'a, WordProps<'a>>) -> Element<'a> {
     let letters = use_atom_ref(cx, LETTERS);
 
-    let rendered_word: String = cx.props.word.chars().map(|c| {
-        if letters.read().contains(&c.to_ascii_lowercase()) {
-            c
-        } else {
-            '_'
-        }
-    }).collect();
+    let rendered_word: String = cx
+        .props
+        .word
+        .chars()
+        .map(|c| {
+            if letters.read().contains(&c.to_ascii_lowercase()) {
+                c
+            } else {
+                '_'
+            }
+        })
+        .collect();
 
     cx.render(rsx!(
         p {
