6423eb5b10c5c00efe9570e9afe115dd81db4f61
Lennart Kloock <lennart.kloock@protonmail.com>
2023-02-28 15:21:26 +0100

    Add single page app endpoint

--- hangman-server/src/main.rs
+++ hangman-server/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use tower_http::{
+    services::{ServeDir, ServeFile},
+    trace::TraceLayer,
+};
+use tracing_subscriber::{filter::LevelFilter, EnvFilter};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .init();
+
+    log::info!("starting hangman server");
+    let app = Router::new()
+        .fallback_service(
+            ServeDir::new("hangman-web/dist")
+                .not_found_service(ServeFile::new("hangman-web/dist/index.html")),
+        )
+        .layer(TraceLayer::new_for_http());
+
+    axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
