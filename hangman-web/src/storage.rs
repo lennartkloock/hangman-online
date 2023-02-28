bcfecdfd13cdced99c1c022bb2c5da2581beab22
Lennart Kloock <lennart.kloock@protonmail.com>
2023-03-01 00:22:24 +0100

    Rename user id to token

--- hangman-web/src/storage.rs
+++ hangman-web/src/storage.rs
@@ -1,20 +1,19 @@
-use rand::RngCore;
-use serde::{Deserialize, Serialize};
-use serde::de::DeserializeOwned;
+use rand::{Rng, RngCore};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use thiserror::Error;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct User {
-    id: String,
     nickname: String,
+    token: String,
 }
 
 impl User {
     pub fn new(nickname: &str) -> Self {
-        let id = rand::thread_rng().next_u64();
+        let token: u64 = rand::thread_rng().gen();
         Self {
-            id: format!("{id:016x}"),
             nickname: nickname.to_string(),
+            token: format!("{token:016x}"),
         }
     }
 }
@@ -33,17 +32,26 @@ pub enum StorageError {
 
 fn retrieve_local_storage() -> Result<web_sys::Storage, StorageError> {
     let window = web_sys::window().ok_or(StorageError::NoWindow)?;
-    window.local_storage().ok().flatten().ok_or(StorageError::NoLocalStorage)
+    window
+        .local_storage()
+        .ok()
+        .flatten()
+        .ok_or(StorageError::NoLocalStorage)
 }
 
 pub fn store<S: Serialize>(key: &str, data: &S) -> Result<(), StorageError> {
     let local_storage = retrieve_local_storage()?;
-    local_storage.set_item(key, &serde_json::to_string(data)?).map_err(|_| StorageError::StoreLoadError)
+    local_storage
+        .set_item(key, &serde_json::to_string(data)?)
+        .map_err(|_| StorageError::StoreLoadError)
 }
 
 pub fn load<T: DeserializeOwned>(key: &str) -> Result<Option<T>, StorageError> {
     let local_storage = retrieve_local_storage()?;
-    if let Some(data) = local_storage.get_item(key).map_err(|_| StorageError::StoreLoadError)? {
+    if let Some(data) = local_storage
+        .get_item(key)
+        .map_err(|_| StorageError::StoreLoadError)?
+    {
         Ok(Some(serde_json::from_str(&data)?))
     } else {
         Ok(None)
