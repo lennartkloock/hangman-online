4eb61210380736b8097956d65376b42275e50406
Lennart Kloock <lennart.kloock@protonmail.com>
2023-02-28 22:57:49 +0100

    Add new crate hangman-data for common types

---
+++ hangman-data/src/game.rs
@@ -0,0 +1,78 @@
+use rand::Rng;
+use serde::{Deserialize, Serialize};
+use std::{
+    fmt::{Display, Formatter},
+    num::ParseIntError,
+    str::FromStr,
+};
+use serde_with::{DeserializeFromStr, SerializeDisplay};
+use thiserror::Error;
+
+/// Two bytes that represent a game code
+///
+/// 4 characters encoded in hex
+#[derive(Copy, Clone, Debug, DeserializeFromStr, Eq, Hash, SerializeDisplay, PartialEq)]
+pub struct GameCode(u16);
+
+impl GameCode {
+    pub fn random() -> Self {
+        Self(rand::thread_rng().gen())
+    }
+}
+
+#[derive(Debug, Error, PartialEq)]
+pub enum ParseGameCodeError {
+    #[error("game code must be 4 characters long")]
+    InvalidLength,
+    #[error("invalid game code: {0}")]
+    ParseIntError(#[from] ParseIntError),
+}
+
+impl FromStr for GameCode {
+    type Err = ParseGameCodeError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() != 4 {
+            return Err(ParseGameCodeError::InvalidLength);
+        }
+        u16::from_str_radix(s, 16)
+            .map(Self)
+            .map_err(ParseGameCodeError::from)
+    }
+}
+
+impl Display for GameCode {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:04X}", self.0)
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum GameLanguage {
+    English,
+    German,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct GameSettings {
+    pub language: GameLanguage,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_code() {
+        let code = GameCode::from_str("1337").unwrap();
+        assert_eq!(code.0, 0x1337);
+        assert_eq!(format!("{}", code), "1337");
+
+        assert_eq!(
+            GameCode::from_str("error"),
+            Err(ParseGameCodeError::InvalidLength)
+        );
+        assert!(GameCode::from_str("error").is_err());
+    }
+}
