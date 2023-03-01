82ad4d5a87ddadcc7b12186c3ae5caf62fc01302
Lennart Kloock <lennart.kloock@protonmail.com>
2023-03-01 11:54:31 +0100

    Remove extractor for UserToken

--- hangman-data/src/user.rs
+++ hangman-data/src/user.rs
@@ -1,8 +1,3 @@
-use axum::{
-    async_trait,
-    extract::FromRequestParts,
-    http::{header, request::Parts, StatusCode},
-};
 use rand::Rng;
 use serde_with::{DeserializeFromStr, SerializeDisplay};
 use std::{
@@ -31,31 +26,5 @@ impl FromStr for UserToken {
 impl Display for UserToken {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "{:016x}", self.0)
-    }
-}
-
-pub struct UserTokenAuthHeader(pub UserToken);
-
-#[async_trait]
-impl<S: Send + Sync> FromRequestParts<S> for UserTokenAuthHeader {
-    type Rejection = (StatusCode, &'static str);
-
-    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
-        match parts
-            .headers
-            .get(header::AUTHORIZATION)
-            .map(|auth| auth.to_str().map(|s| UserToken::from_str(s)))
-        {
-            Some(Ok(Ok(token))) => Ok(Self(token)),
-            Some(Err(_)) => Err((
-                StatusCode::BAD_REQUEST,
-                "`Authorization` header contains invalid characters",
-            )),
-            Some(Ok(Err(_))) => Err((
-                StatusCode::BAD_REQUEST,
-                "`Authorization` header must contain a valid user token",
-            )),
-            None => Err((StatusCode::BAD_REQUEST, "`Authorization` header is missing")),
-        }
     }
 }
