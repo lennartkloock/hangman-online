2089b03dc0f8c064655e43af324d22651ae7fc43
Lennart Kloock <lennart.kloock@protonmail.com>
2023-03-08 22:11:02 +0100

    Refactor message handler

--- hangman-web/src/game/ongoing_game/game_logic.rs
+++ hangman-web/src/game/ongoing_game/game_logic.rs
@@ -1,34 +1,36 @@
 use crate::game::ongoing_game::GameState;
 use dioxus::prelude::*;
-use hangman_data::ServerMessage;
+use hangman_data::{Game, ServerMessage};
 use log::{info, warn};
 
 pub fn handle_message(msg: ServerMessage, state: &UseRef<GameState>) {
     match msg {
         ServerMessage::Init(data) => state.set(GameState::Joined(data)),
         ServerMessage::UpdatePlayers(players) => state.with_mut(|s| {
-            if let GameState::Joined(game) = s {
+            modify_game(s, |game| {
                 info!("updating player list: {players:?}");
                 game.players = players;
-            } else {
-                warn!("received update message before init");
-            }
+            });
         }),
         ServerMessage::NewMessage(msg) => state.with_mut(|s| {
-            if let GameState::Joined(game) = s {
+            modify_game(s, |game| {
                 info!("new chat message: {msg:?}");
                 game.chat.push(msg);
-            } else {
-                warn!("received update message before init");
-            }
+            });
         }),
         ServerMessage::UpdateTriesUsed(tries_used) => state.with_mut(|s| {
-            if let GameState::Joined(game) = s {
+            modify_game(s, |game| {
                 info!("updating tries used: {tries_used}");
                 game.tries_used = tries_used;
-            } else {
-                warn!("received update message before init");
-            }
+            });
         }),
+    }
+}
+
+fn modify_game<F: FnOnce(&mut Game)>(state: &mut GameState, f: F) {
+    if let GameState::Joined(game) = state {
+        f(game)
+    } else {
+        warn!("received update message before init");
     }
 }
