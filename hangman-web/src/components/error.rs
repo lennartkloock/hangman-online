8e665882631cb0d3f33906a8339e4912ab59dc69
Lennart Kloock <lennart.kloock@protonmail.com>
2023-03-08 20:13:19 +0100

    Add RcError component

--- hangman-web/src/components/error.rs
+++ hangman-web/src/components/error.rs
@@ -2,7 +2,7 @@ use crate::components::CenterContainer;
 use dioxus::prelude::*;
 use dioxus_material_icons::{MaterialIcon, MaterialIconColor};
 use log::error;
-use std::error::Error;
+use std::{convert::Infallible, error::Error, fmt::Display, rc::Rc};
 
 #[derive(Props)]
 pub struct ErrorProps<'a, E: Error> {
@@ -11,22 +11,7 @@ pub struct ErrorProps<'a, E: Error> {
 }
 
 pub fn Error<'a, E: Error>(cx: Scope<'a, ErrorProps<'a, E>>) -> Element<'a> {
-    let details = cx.props.error.as_ref().map(|e| {
-        cx.render(rsx!(
-            button {
-                class: "text-xs hover:underline",
-                onclick: move |_| {
-                    if let Some(w) = web_sys::window() {
-                        if let Err(e) = w.alert_with_message(&format!("Error: {e}")) {
-                            error!("Error calling window.alert: {:?}", e);
-                        }
-                    }
-                },
-                "Show details"
-            }
-        ))
-    });
-
+    let details = cx.props.error.as_ref().map(|e| render_details(cx.scope, e));
     cx.render(rsx!(
         CenterContainer {
             MaterialIcon { name: "warning", color: MaterialIconColor::Light, size: 200 }
@@ -35,6 +20,42 @@ pub fn Error<'a, E: Error>(cx: Scope<'a, ErrorProps<'a, E>>) -> Element<'a> {
                 "{cx.props.title}"
             }
             details
+        }
+    ))
+}
+
+#[derive(Props)]
+pub struct RcErrorProps<'a, E: Error> {
+    title: &'a str,
+    error: Option<Rc<E>>,
+}
+
+pub fn RcError<'a, E: Error>(cx: Scope<'a, RcErrorProps<'a, E>>) -> Element<'a> {
+    let details = cx.props.error.as_ref().map(|e| render_details(cx.scope, e));
+    cx.render(rsx!(
+        CenterContainer {
+            MaterialIcon { name: "warning", color: MaterialIconColor::Light, size: 200 }
+            h1 {
+                class: "text-3xl",
+                "{cx.props.title}"
+            }
+            details
+        }
+    ))
+}
+
+fn render_details<'a, D: Display  + 'a>(cx: &'a ScopeState, error: D) -> Element {
+    cx.render(rsx!(
+        button {
+            class: "text-xs hover:underline",
+            onclick: move |_| {
+                if let Some(w) = web_sys::window() {
+                    if let Err(e) = w.alert_with_message(&format!("Error: {error}")) {
+                        error!("Error calling window.alert: {e:?}");
+                    }
+                }
+            },
+            "Show details"
         }
     ))
 }
